20ca0fb69a1a6fa8a35bfd44d96432972cebc43e
Yakumo Saki <yakumo@ziomatrix.org>
2021-09-08 16:25:42 +0900

    add kill waybar

---
+++ scripts/kill_waybar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+LOG=/tmp/kill_waybar.log
+RET=`pgrep -x waybar | wc -l`
+
+echo "`date` START Kill duplicate waybar" >> $LOG
+
+sleep 2
+
+if [ $RET -ge 2 ]; then
+    OLD_PID=`pgrep -x waybar | head -n 1`
+    echo "Kill old waybar ${OLD_PID}" >> $LOG
+    kill -9 $OLD_PID
+else
+    echo "No duplicate waybar :)" >> $LOG
+fi
+
+echo "`date` END" >> $LOG
