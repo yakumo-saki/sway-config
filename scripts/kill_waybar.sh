8f422fdac0732cb8684cf8305d583749d900b166
Yakumo Saki <yakumo@ziomatrix.org>
2021-10-19 16:55:54 +0900

    commit

--- scripts/kill_waybar.sh
+++ scripts/kill_waybar.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
 LOG=/tmp/kill_waybar.log
-RET=`pgrep -x waybar | wc -l`
+
+function kill_old_waybar () {
+    RET=`pgrep -x waybar | wc -l`
+    if [ $RET -ge 2 ]; then
+        OLD_PID=`pgrep -x waybar | head -n 1`
+        echo "[attempt $1] Kill old waybar ${OLD_PID}" >> $LOG
+        kill -9 $OLD_PID
+    else
+        echo "[attempt $1] No duplicate waybar :)" >> $LOG
+    fi
+}
 
 echo "`date` START Kill duplicate waybar" >> $LOG
 
 sleep 5
+kill_old_waybar 1
 
-if [ $RET -ge 2 ]; then
-    OLD_PID=`pgrep -x waybar | head -n 1`
-    echo "Kill old waybar ${OLD_PID}" >> $LOG
-    kill -9 $OLD_PID
-else
-    echo "No duplicate waybar :)" >> $LOG
-fi
+sleep 5
+kill_old_waybar 2
+
+sleep 5
+kill_old_waybar 3
 
 echo "`date` END" >> $LOG
---
+++ scripts/move_vmwin10_workspace.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sleep 1
+wlrctl window focus title:VMWIN10
+wtype -M Logo -M Shift ^
+
