8d7e95455c6a978f85e4e14b6a0132aefa7944a1
Yakumo Saki <yakumo@ziomatrix.org>
2022-03-23 12:54:06 +0900

    fix some bugs

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eu
 
 ## FROM https://raw.githubusercontent.com/yschaeff/sway_screenshots/master/screenshot.sh
 
@@ -9,11 +9,23 @@ MENU="rofi -i -dmenu"
 RECORDER=wf-recorder
 TARGET=$(xdg-user-dir PICTURES)/screenshots
 TARGET_VIDEOS=$(xdg-user-dir PICTURES)/screencasts
-NOTIFY=$(pidof mako || pidof dunst) || true
+#NOTIFY=$(pidof mako || pidof dunst) || true
+NOTIFY=true
 FOCUSED=$(swaymsg -t get_tree | jq '.. | ((.nodes? + .floating_nodes?) // empty) | .[] | select(.focused and .pid) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')
 OUTPUTS=$(swaymsg -t get_outputs | jq -r '.[] | select(.active) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')
 WINDOWS=$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')
 REC_PID=$(pidof $RECORDER) || true
+
+# recoding filename temp file.
+REC_TMP_FILE="/tmp/__screenrecording_filename__.txt"
+
+FILENAME="$TARGET/$(date +'%Y%m%d_%H%M%S_screenshot.png')"
+RECORDING_FILENAME="$(date +'%Y%m%d_%H%M%S_recording.mp4')"
+RECORDING="${TARGET_VIDEOS}/${RECORDING_FILENAME}"
+
+# make sure directory exists
+mkdir -p $TARGET
+mkdir -p $TARGET_VIDEOS
 
 # 12x6+0.5+0 GIMP
 # -unsharp 10x5+0.7+0 よき
@@ -32,10 +44,23 @@ notify() {
     fi
 }
 
+notify-rec() {
+    notify "Screen Recording" "Target: $1"
+    rm -f $REC_TMP_FILE
+    echo "$RECORDING_FILENAME" > $REC_TMP_FILE
+}
+
 if [ ! -z $REC_PID ]; then
-    echo pid: $REC_PID
+    echo "pid: $REC_PID"
     kill -SIGINT $REC_PID
-    notify "Screen recorder stopped" -t 2000
+    
+    if [ -e $REC_TMP_FILE ]; then
+        REC_FILENAME=`cat $REC_TMP_FILE`
+        notify "End Recoding" "Recorded: \n$REC_FILENAME" -t 2000
+    else
+        notify "End Recoding" "Recorded" -t 2000
+    fi
+
     exit 0
 fi
 
@@ -65,11 +90,6 @@ else
   echo "From param $1"
   CHOICE=$1
 fi
-
-mkdir -p $TARGET
-mkdir -p $TARGET_VIDEOS
-FILENAME="$TARGET/$(date +'%Y%m%d_%H%M%S_screenshot.png')"
-RECORDING="$TARGET_VIDEOS/$(date +'%Y-%m-%d_%Hh%Mm%Ss_recording.mp4')"
 
 case "$CHOICE" in
     "Fullscreen")
@@ -102,17 +122,26 @@ case "$CHOICE" in
         notify "Color Picker" "Color picked is\n${COLOR} Copied to clipboard" -t 6000
         exit;;
     "Record-select-output")
-        $RECORDER -g "$(echo "$OUTPUTS"|slurp)" -f "$RECORDING"
+        $RECORDER -g "$(echo "$OUTPUTS"|slurp)" -f "$RECORDING" &
+        notify-rec "Display"
+
         REC=1 ;;
     "Record-select-window")
-        $RECORDER -g "$(echo "$WINDOWS"|slurp)" -f "$RECORDING"
+        PARAM="$(echo "$WINDOWS"|slurp)"
+        $RECORDER -g "$(echo "$WINDOWS"|slurp)" -f "$RECORDING" &
+        notify-rec "Window"
       
         REC=1 ;;
     "Record-region")
-        $RECORDER -g "$(slurp)" -f "$RECORDING"
+        PARAM="$(slurp)"
+        $RECORDER -g "${PARAM}" -f "$RECORDING" &
+        notify-rec "Region"
+
         REC=1 ;;
     "Record-focused")
-        $RECORDER -g "$(eval echo $FOCUSED)" -f "$RECORDING"
+        $RECORDER -g "$(eval echo $FOCUSED)" -f "$RECORDING" &
+        notify-rec "Focused"
+
         REC=1 ;;
     *)
         grim -g "$(eval echo $CHOICE)" "$FILENAME" ;;
@@ -120,10 +149,9 @@ esac
 
 
 if [ $REC ]; then
-    notify "Recording" "Recording stopped: $RECORDING" -t 10000
-    wl-copy < $RECORDING
+    echo "rec start"
 else
-    notify "Screenshot" "File saved as \n$FILENAME\nand copied to clipboard" -t 6000 -i $FILENAME
+    notify "Screenshot saved" "File saved as \n$FILENAME\nand copied to clipboard" -t 6000 -i $FILENAME
     wl-copy < $FILENAME
     # feh $FILENAME
 fi
